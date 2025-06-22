931800ce1c2660c9ae286cc615f3423eae9d0197
Bigbear2006 <morozm982@gmail.com>
2025-06-23 00:39:17 +0300

    Fix TypeError: can't compare offset-naive and offset-aware datetimes

--- backend/bot/services/lunar_nodes.py
+++ backend/bot/services/lunar_nodes.py
@@ -1,13 +1,15 @@
-from datetime import datetime
+from datetime import datetime, UTC
+
+from django.utils.timezone import now
 
 from bot.text_templates.lunar_nodes import lunar_nodes
 
 
 def get_lunar_nodes(date: datetime = None) -> dict[str, str] | None:
     if date is None:
-        date = datetime.now()
+        date = now()
     for date_range, nodes in lunar_nodes.items():
         start_date, end_date = date_range
-        if start_date <= date <= end_date:
+        if start_date.replace(tzinfo=UTC) <= date <= end_date.replace(tzinfo=UTC):
             return nodes
     return None
